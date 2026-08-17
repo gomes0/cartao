70b5a1147bd01bf8d61769a04592a7c43dc94340
Elias Gomes <eliasgomesduraes2008@gmail.com>
2026-08-17 20:25:20 -0300

    PWA Update.

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = "cartao-elias-v1";
+
+const ARQUIVOS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./css/style.css",
+    "./img/fotoelias.jpg",
+    "./img/icon.png"
+];
